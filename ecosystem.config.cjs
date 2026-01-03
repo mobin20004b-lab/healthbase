911cc87cc3dd43a9f045dcd64a6ebfc79997926f
saeed <mobin.20004b@gmail.com>
2026-01-03 19:57:09 +0330

    Add initial UI/UX documentation file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,9 +2,17 @@ module.exports = {
   apps: [
     {
       name: "topmedica",
+      cwd: "/opt/healthbase",
       script: "bun",
       args: "start",
       interpreter: "none",
+      env_file: "/opt/healthbase/.env",
+      instances: 1,
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 1000,
+      merge_logs: true,
+      time: true,
     },
   ],
 };
